d2a4948b2cc8d74d493747a639cda6cbf3878a59
RayTan406 <112354651+RayTan406@users.noreply.github.com>
2024-02-27 18:58:42 -0500

    status code in response

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from flask import Flask, request, render_template
-app = Flask(__name__)
- 
-@app.route("/")
-def root():
-    return render_template("index.html")
+from flask import Flask, render_template
 
-if __name__ == "__main__":
-    app.run(debug=True,host='0.0.0.0',port=8080)
+app = Flask(__name__)
+
+@app.route('/')
+def root():
+    return 'Hello, World!'
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=8080)
