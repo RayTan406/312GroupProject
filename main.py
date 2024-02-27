2a0d687e4dd4452ab6db4288f24ddab12577eb8c
RayTan406 <112354651+RayTan406@users.noreply.github.com>
2024-02-27 18:14:59 -0500

    Initial flask commit

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-from flask import Flask
+from flask import Flask, request, render_template
+app = Flask(__name__,template_folder="./public")
+ 
+@app.route("/")
+def root():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True,host='0.0.0.0',port=8080)
