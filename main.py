53fd045eecdb0a078d609e8c5b09eff749c2dc8f
RayTan406 <112354651+RayTan406@users.noreply.github.com>
2024-02-27 19:01:05 -0500

    fixed wrong file upload

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
-from flask import Flask, render_template
-
+from flask import Flask, request, render_template, make_response
 app = Flask(__name__)
-
-@app.route('/')
+ 
+@app.route("/")
 def root():
-    return 'Hello, World!'
+    return render_template("index.html")
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8080)
+@app.after_request
+def nosniff(response):
+    if(request.path == "/static/clue.JPG" or request.path == "/static/style.css"):
+        response.headers['X-Content-Type-Options'] = 'nosniff'
+        response.status_code = 200
+        return response
+    print(request.path)
+    response.headers['X-Content-Type-Options'] = 'nosniff'
+    response.status_code = 200
+    return response
+if __name__ == "__main__":
+    app.run(debug=True,host='0.0.0.0',port=8080)
