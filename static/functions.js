707ac95e7eb65b112c8381d406eab6ad51a253a5
Marcell_Kovats <mbkovats@buffalo.edu>
2024-03-07 16:06:25 -0500

    added background buttons

---
+++ static/functions.js
@@ -0,0 +1,8 @@
+function backgroundChangeToRed() {
+    document.body.style.backgroundColor = "red";
+}
+function backgroundChangeToBlue() {
+    document.body.style.backgroundColor = "#87d6d4";
+}
+document.getElementById("background_buttonred").addEventListener('click', backgroundChangeToRed(), false);
+document.getElementById("background_buttonblue").addEventListener('click', backgroundChangeToBlue(), false);
